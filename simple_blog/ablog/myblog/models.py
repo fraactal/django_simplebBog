5945e4ce07b69dcd5ae25b704b77d2b546d27188
dtjonathanvaldes <jonathan.valdes@darts-ti.com>
2024-05-22 18:54:07 -0400

    agregué una clase modelo, chaya

--- simple_blog/ablog/myblog/models.py
+++ simple_blog/ablog/myblog/models.py
@@ -31,3 +31,85 @@ class Post(models.Model):
     def get_absolute_url(self):
         #return reverse('article_detail', args=(str(self.id)))
         return reverse('home')
+    
+class Consumos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    alcance = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=50)
+    subcategoria = models.CharField(max_length=100)
+    fuente = models.CharField(max_length=255)
+    cantidad_fuente = models.CharField(max_length=255)
+    campus = models.CharField(max_length=255)
+    linkRespaldo = models.CharField(max_length=255)
+    comentarios = models.CharField(max_length=255)
+    huellaChile = models.CharField(max_length=255)
+
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+    
+class Proyectos(models.Model):
+    id = models.IntegerField(primary_key=True)
+    consumo = models.CharField(max_length=255)
+    HuellaDeCarbono = models.CharField(max_length=255)
+    HuellaHidrica = models.CharField(max_length=255)
+    HuellaFinanciera = models.CharField(max_length=255)
+
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Alcances(models.Model):
+    descripcion = models.CharField(max_length=255)
+    categorias = models.CharField(max_length=255)
+    tipoDeAlcance = models.CharField(max_length=255)
+
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    ##subcategorias = models.CharField(max_length=255) ### Quizas este campo no va
+    
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
+class SubCategorias(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    categorias = models.CharField(max_length=255) 
+    
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+    
+class Fuente(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255)
+    combustible = models.CharField(max_length=255)
+    UnidadFuente = models.CharField(max_length=255)
+    FactorEmision = models.CharField(max_length=255)
+    Alcance = models.CharField(max_length=255)
+    Categoria = models.CharField(max_length=255)
+    Subcategoria = models.CharField(max_length=255) 
+    
+    def __str__ (self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home')
+
